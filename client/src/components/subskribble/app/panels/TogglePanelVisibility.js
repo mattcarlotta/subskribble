bf67d5aafa104ab44e774136f669d779b3141cc4
Matt Carlotta <carlotta.matt@gmail.com>
2018-02-03 09:21:00 -0800

    Simplify TogglePanelVisibility component props

---
+++ client/src/components/subskribble/app/panels/TogglePanelVisibility.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Button } from 'antd';
+
+export default WrappedComponent => {
+  class TogglePanelVisibility extends Component {
+    state = { visible: true };
+
+    switchVisibility = () => this.setState({ visible: !this.state.visible })
+
+    buttonPanel = (visible) => (
+      <Button className="panel-button" onClick={this.switchVisibility}>
+      { visible
+        ? <i className="material-icons">remove</i>
+        : <i className="material-icons">add</i>
+      }
+      </Button>
+    )
+    render() {
+      return (
+        <WrappedComponent
+          {...this.props}
+          buttonPanel={this.buttonPanel}
+          visible={this.state.visible}
+        />
+      )
+    }
+  }
+  return TogglePanelVisibility;
+}
