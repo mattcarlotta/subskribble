439ec0eeea3969c9545d195029e4db57a00f6e3c
Matt <carlotta.matt@gmail.com>
2019-03-06 15:25:32 -0800

    Add appActions tests

---
+++ client/src/actions/__tests__/appActions.test.js
@@ -0,0 +1,29 @@
+import { resetServerMessages, serverErrorMessage } from '../appActions.js';
+
+const initialState = {
+  error: '',
+};
+
+describe('App Actions', () => {
+  let store;
+  let serverError;
+  beforeEach(() => {
+    store = createStoreFactory();
+    serverError = 'Error';
+  });
+
+  it('adds a serverError to redux state', async () => {
+    await store.dispatch(serverErrorMessage(serverError));
+    const {
+      server: { error },
+    } = store.getState();
+    expect(error).toBe(serverError);
+  });
+
+  it('removes all server messages from redux state', async () => {
+    await store.dispatch(serverErrorMessage(serverError));
+    await store.dispatch(resetServerMessages());
+    const { server } = store.getState();
+    expect(server).toEqual(initialState);
+  });
+});
